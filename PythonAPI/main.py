05e06699e4666bfc44c73b6d347aded6fabca352
ericthered1138 <ericjennings955@gmail.com>
2022-01-14 16:41:31 -0600

    Rough python project organization

---
+++ PythonAPI/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+# Setup logging
+import logging
+logging.basicConfig(filename="records.log", level=logging.DEBUG,
+                    format="[%(levelname)s] - %(asctime)s - %(name)s - : %(message)s in %(pathname)s:%(lineno)d")
+
+# Setup flask
+app: Flask = Flask(__name__)
+CORS(app)
+
+
+app.run()
