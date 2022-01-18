90c5fd2bea2a2d732a56319d9209ac7fdf2e98a5
Chris24xx <94495502+Chris24xx@users.noreply.github.com>
2022-01-18 18:54:46 -0500

    Solution to groups table

--- PythonAPI/entities/group.py
+++ PythonAPI/entities/group.py
@@ -4,24 +4,18 @@ class Group:
                  user_id: int = None,  # the original user that created the group
                  group_name: str = None,  # must be unique
                  group_about: str = None,
-                 group_image_data: str = None,
-                 group_members: list[str] = None
                  ):
 
         self.group_id = group_id
         self.user_id = user_id
         self.group_name = group_name
         self.group_about = group_about
-        self.group_image_data = group_image_data
-        self.group_members = group_members
 
     def make_dictionary(self):
         dictionary = {
             "group_id": self.group_id,
             "user_id": self.user_id,
             "group_name": self.group_name,
-            "group_about": self.group_about,
-            "group_image_data": self.group_image_data,
-            "group_members": self.group_members
+            "group_about": self.group_about
             }
         return dictionary
