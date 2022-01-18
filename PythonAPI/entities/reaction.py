d2f18b1ba959ece72ed64e01b1e7ce2611a4acd5
ericthered1138 <ericjennings955@gmail.com>
2022-01-18 14:02:34 -0600

    Update reaction.py

--- PythonAPI/entities/reaction.py
+++ PythonAPI/entities/reaction.py
@@ -1,12 +1,15 @@
 class Reaction:
     def __init__(self,
+                 reaction_id: int = None,
                  post_id: int = None,
-                 likes: int = None,
-                 dislikes: int = None,
+                 reactor_id: int = None,
+                 reaction_status: str = None,
                  ):
 
-        self.likes = likes
-        self.dislikes = dislikes
+        self.reaction_id = reaction_id
+        self.post_id = post_id
+        self.reactor_id = reactor_id
+        self.reaction_status = reaction_status
 
     def make_dictionary(self):
         dictionary = {
