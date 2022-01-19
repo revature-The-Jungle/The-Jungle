03e932bf70c8127637b18d4f7520e1242345ea92
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-01-19 09:23:57 -0800

    minor changes

--- JavaAPI/src/test/java/dao/userDAOTests.java
+++ JavaAPI/src/test/java/dao/userDAOTests.java
@@ -1,6 +1,8 @@
 package dao;
 
 import dev.com.thejungle.dao.UserDAO;
+import dev.com.thejungle.dao.UserDAOImp;
+import dev.com.thejungle.entity.User;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,7 +12,10 @@ public class userDAOTests {
 
     @Test
     void testGetUserByUsername() {
-        JungleUser newJungleUser = jungleUserDAO.getUserByUsername("");
+        User newJungleUser = userDAO.getUserByUsername("");
         System.out.println("new user is " + newJungleUser);
         Assert.assertEquals(newJungleUser.getUsername(), "");
 }
+
+
+
