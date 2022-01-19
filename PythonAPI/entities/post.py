a50e89ffa0918c7771599e33a14ab4ba41e959e5
VictorFloresHerrera <victor.floresherrera@revature.net>
2022-01-19 11:17:35 -0600

    not important

---
+++ PythonAPI/entities/post.py
@@ -0,0 +1,30 @@
+class Post:
+    def __init__(self,
+                 post_id: int = None,  # primary key
+                 user_id: int = None,  # foreign key
+                 username: str = None,
+                 post_text: str = None,
+                 youtube_url: str = None,
+                 image_data: str = None,
+                 date_time_of_creation: str = None
+                 ):
+
+        self.post_id = post_id
+        self.user_id = user_id
+        self.username = username
+        self.post_text = post_text
+        self.youtube_url = youtube_url
+        self.image_data = image_data
+        self.date_time_of_creation = date_time_of_creation
+
+    def make_dictionary(self):
+        dictionary = {
+            "post_id": self.post_id,
+            "user_id": self.user_id,
+            "username": self.username,
+            "post_text": self.post_text,
+            "youtube_url": self.youtube_url,
+            "image_data": self.image_data,
+            "date_time_of_creation": self.date_time_of_creation
+            }
+        return dictionary
