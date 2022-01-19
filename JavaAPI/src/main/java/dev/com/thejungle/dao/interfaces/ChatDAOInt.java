56b3673f8ec83968c7ae8d6360bdb1bf2f33c546
Hyungsuk Kim <76229107+sesame0717@users.noreply.github.com>
2022-01-19 14:20:27 -0800

    implemented getMessageHistory DAO/Service and its tests

--- JavaAPI/src/main/java/dev/com/thejungle/dao/interfaces/ChatDAOInt.java
+++ JavaAPI/src/main/java/dev/com/thejungle/dao/interfaces/ChatDAOInt.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 
 public interface ChatDAOInt {
 
-    ChatMessage createMessage(int chatId, int userId, String chatDate, int group_id, String chatContent);
+    ChatMessage createMessage(int chatId, String chatDate, int userId, int groupId, String chatContent);
+
+    ArrayList<ChatMessage> getMessageHistory(int groupId);
 
     ArrayList<ChatMessage> getMessageHistory();
 }
