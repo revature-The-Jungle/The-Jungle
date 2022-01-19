8ccbadcb406d6aaf535cce62b75b4cecb29d1c3c
Chris24xx <94495502+Chris24xx@users.noreply.github.com>
2022-01-19 12:35:59 -0500

    leaving group and grabbing users list for a group

--- PythonAPI/entities/group_member_junction.py
+++ PythonAPI/entities/group_member_junction.py
@@ -1,14 +1,12 @@
-class UserGroup:
+class GroupMemberJunction:
 
-    def __init__(self, p_id: int, user_id: int, group_id: int):
+    def __init__(self, user_id: int, group_id: int):
         self.group_id = group_id
         self.user_id = user_id
-        self.p_id = p_id
 
     def make_dictionary(self):
         dictionary = {
             "group_id": self.group_id,
             "user_id": self.user_id,
-            "p_id": self.p_id,
         }
         return dictionary
