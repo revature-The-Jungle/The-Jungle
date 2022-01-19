5bddca3bbcacf9a5873acef4bce843f6ad1ed18f
ericthered1138 <ericjennings955@gmail.com>
2022-01-19 16:11:30 -0600

    1

--- PythonAPI/data_access_layer/implementation_classes/create_post_dao_imp.py
+++ PythonAPI/data_access_layer/implementation_classes/create_post_dao_imp.py
@@ -1,3 +1,5 @@
+import base64
+
 from data_access_layer.abstract_classes.create_post_dao import CreatePostDAO
 from entities.post import Post
 
@@ -6,9 +8,10 @@ class CreatePostDAOImp(CreatePostDAO):
 
     def create_post(self, post: Post) -> Post:
         """a method to create a post in the database"""
-
         pass
 
-    def create_post_image(self, image: str) -> bool:
+    def create_post_image(self, user_id: int, image: str) -> bool:
         """a method to place an image into the database"""
-        pass
+        blob = base64.standard_b64encode(image)
+        sql = "INSERT INTO employee_picture_table VALUES (default, ?, ?)"
+
