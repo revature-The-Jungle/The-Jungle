c3ddbe80c888b5d8e8326898f818e83b95162ffa
Irfan Uludag <ozgr329@gmail.com>
2022-01-19 17:24:01 -0500

    saving work

--- JavaAPI/src/main/java/dev/com/thejungle/app/appcontroller/controllers/ChatController.java
+++ JavaAPI/src/main/java/dev/com/thejungle/app/appcontroller/controllers/ChatController.java
@@ -1,7 +1,30 @@
 package dev.com.thejungle.app.appcontroller.controllers;
 
+import dev.com.thejungle.dao.implementations.ChatDAO;
+import dev.com.thejungle.service.implementations.ChatService;
+import io.javalin.http.Handler;
+
 public class ChatController {
+    ChatService chatService;
 
-    // make handlers
+    public ChatController(ChatService chatService){
+        this.chatService = chatService;
+    }
 
+    public Handler connectTochat =  ws -> {
+        ws.onConnect(ctx -> {
+
+            String username = "User"  ;
+            userUsernameMap.put(ctx, ctx.pathParam("id"));
+            broadcastMessage(userUsernameMap.get(ctx), ctx.getSessionId() ,ctx.pathParam("id"));
+        });
+        ws.onClose(ctx -> {
+            String username = userUsernameMap.get(ctx);
+            userUsernameMap.remove(ctx);
+            broadcastMessage(userUsernameMap.get(ctx), ctx.getSessionId() ,ctx.pathParam("id"));
+        });
+        ws.onMessage(ctx -> {
+            broadcastMessage(userUsernameMap.get(ctx), ctx.message() ,ctx.pathParam("id"));
+        });
+    });
 }
