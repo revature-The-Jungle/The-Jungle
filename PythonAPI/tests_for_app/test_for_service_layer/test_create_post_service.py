92caa00cf4a25d4411a478c29c6373ef49e7d612
ericthered1138 <ericjennings955@gmail.com>
2022-01-20 15:51:07 -0600

    Update test_create_post_service.py

--- PythonAPI/tests_for_app/test_for_service_layer/test_create_post_service.py
+++ PythonAPI/tests_for_app/test_for_service_layer/test_create_post_service.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch, Mock
 
 from data_access_layer.abstract_classes.create_post_dao import CreatePostDAO
 from data_access_layer.implementation_classes.create_post_dao_imp import CreatePostDAOImp
@@ -10,7 +10,8 @@ create_post_service: CreatePostService = CreatePostServiceImp(create_post_dao)
 
 
 def test_create_post_image_service_success():
-    pass
+    create_post_dao.create_post_image = MagicMock(return_value="thisisareturnvalue")
+    assert create_post_service.create_post_image_service(1, "thisisaforwardvalue")
 
 
 def test_create_post_image_service_failure_not_int():
