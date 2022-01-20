bc23fa372b252059fbf47d5fae6b0b0da0189883
ericthered1138 <ericjennings955@gmail.com>
2022-01-19 23:49:17 -0600

    added some image data access methods

--- PythonAPI/data_access_layer/implementation_classes/user_profile_dao_imp.py
+++ PythonAPI/data_access_layer/implementation_classes/user_profile_dao_imp.py
@@ -1,5 +1,6 @@
 from data_access_layer.abstract_classes.user_profile_dao import UserProfileDAO
 from entities.user import User
+from util.database_connection import connection
 
 
 class UserProfileDAOImp(UserProfileDAO):
@@ -8,10 +9,29 @@ class UserProfileDAOImp(UserProfileDAO):
         pass
 
     def get_user_image(self, user_id: int) -> str:
-        pass
+        """a method to get a user image from the database"""  # need to create a custom exception and database checker
+        cursor = connection.cursor()
+        sql = f"select picture from user_picture_table where user_id = {user_id};"
+        cursor.execute(sql)
+        image = cursor.fetchone()[0]
+        image_decoded = image.decode('utf-8')
+        return image_decoded
 
-    def update_user_image(self, user_id: int, image: str) -> bool:
-        pass
+    def update_user_image(self, user_id: int, image: str) -> str:
+        """a method to place a user image into the database"""
+        # delete any existing image from the database and place the image in the database
+        cursor = connection.cursor()
+        sql = f"DELETE FROM user_picture_table where user_id = {user_id}; " \
+              f"INSERT INTO user_picture_table VALUES (default, {user_id}, '{image}');"
+        cursor.execute(sql)
+        connection.commit()
+
+        # get the new image and send it back up to the service layer
+        sql = f"select picture from user_picture_table where user_id = {user_id};"
+        cursor.execute(sql)
+        image = cursor.fetchone()[0]
+        image_decoded = image.decode('utf-8')
+        return image_decoded
 
     def update_image_format(self, user_id: int, image_data: str) -> User:
         pass
--- PythonAPI/tests_for_app/test_for_data_access_layer/test_create_post_dao.py
+++ PythonAPI/tests_for_app/test_for_data_access_layer/test_create_post_dao.py
@@ -1,8 +1,19 @@
-from data_access_layer.implementation_classes.create_post_dao_imp import CreatePostDAOImp
+from psycopg.errors import ForeignKeyViolation
 
-create_post_dao = CreatePostDAOImp()
+from data_access_layer.implementation_classes.create_post_dao_imp import CreatePostDAOImp, CreatePostDAO
+
+create_post_dao: CreatePostDAO = CreatePostDAOImp()
 
 
 def test_create_post_image_success():
-    """a method to place an image into the database"""
-    assert create_post_dao.create_post_image(10_000, "this is a test")
+    """tests by using an existing"""
+    assert create_post_dao.create_post_image(9000, "thisisahappytest")
+
+
+# def test_create_post_image_failure():
+#     """tests by using a post id that can't possibly exist"""
+#     try:
+#         create_post_dao.create_post_image(-5, "this is a sad test")
+#         assert False
+#     except ForeignKeyViolation:
+#         assert True
