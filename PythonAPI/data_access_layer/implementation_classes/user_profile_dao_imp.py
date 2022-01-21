77e94ec5c961a945672384a3a99fd03f080888af
dZazulak <94470265+dZazulak@users.noreply.github.com>
2022-01-21 11:04:37 -0600

    Added test to User Profile Service layer
    Added two new custom exceptions as well as a success test and three failure tests

--- PythonAPI/data_access_layer/implementation_classes/user_profile_dao_imp.py
+++ PythonAPI/data_access_layer/implementation_classes/user_profile_dao_imp.py
@@ -17,7 +17,7 @@ class UserProfileDAOImp(UserProfileDAO):
         return user
 
     def update_user_profile(self, user: User) -> User:
-        """ a method used to update information for the profile besides the image"""
+        """ A method used to update information for the profile besides the image"""
 
         sql = "select * from user_table where user_id = %s"
         cursor = connection.cursor()
