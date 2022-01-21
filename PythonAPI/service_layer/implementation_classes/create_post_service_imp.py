64d2d3f68e28f1d72d8cae1df875821a1602acde
ericthered1138 <ericjennings955@gmail.com>
2022-01-21 10:35:48 -0600

    Revert "Merge remote-tracking branch 'origin/dZazulak/python/profile_create_post/sp1' into eJennings/python/userprofile/sprint1"
    This reverts commit 846d21b2febbefce3ddcd7ec233684d00af1c948, reversing
    changes made to 8451f75eb0d9382252d1afa4767265a917260acf.

--- PythonAPI/service_layer/implementation_classes/create_post_service_imp.py
+++ PythonAPI/service_layer/implementation_classes/create_post_service_imp.py
@@ -1,3 +1,5 @@
+from custom_exceptions.image_must_be_a_string import ImageMustBeAString
+from custom_exceptions.post_id_must_be_an_integer import PostIdMustBeAnInteger
 from entities.post import Post
 from service_layer.abstract_classes.create_post_service import CreatePostService
 from data_access_layer.implementation_classes.create_post_dao_imp import CreatePostDAOImp
@@ -10,8 +12,17 @@ class CreatePostServiceImp(CreatePostService):
 
     def create_post_service(self, post: Post) -> Post:
         """not created yet"""
+        # check to make sure that there is a user_id in the database user_table
         pass
 
-    def create_post_image_service(self, post_id: int, image: str) -> bool:
+    def create_post_image_service(self, post_id: int, image: str) -> str:
         """service layer image checks"""
+        # Check to make sure that the post_id is an integer
+        if not str(post_id).isnumeric():
+            raise PostIdMustBeAnInteger("The post id must be an integer.")
+
+        # Check to make sure that the image is a string
+        if not type(image) == str or not image:
+            raise ImageMustBeAString("The image must be a string format.")
+
         return self.create_post_dao.create_post_image(post_id, image)
