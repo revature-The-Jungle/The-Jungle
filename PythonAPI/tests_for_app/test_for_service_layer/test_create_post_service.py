92b70d8b5cac73202d78c02170525809a6512787
ericthered1138 <ericjennings955@gmail.com>
2022-01-20 22:24:07 -0600

    Update test_create_post_service.py

--- PythonAPI/tests_for_app/test_for_service_layer/test_create_post_service.py
+++ PythonAPI/tests_for_app/test_for_service_layer/test_create_post_service.py
@@ -1,5 +1,7 @@
 from unittest.mock import MagicMock, patch, Mock
 
+from custom_exceptions.image_must_be_a_string import ImageMustBeAString
+from custom_exceptions.post_id_must_be_an_integer import PostIdMustBeAnInteger
 from data_access_layer.abstract_classes.create_post_dao import CreatePostDAO
 from data_access_layer.implementation_classes.create_post_dao_imp import CreatePostDAOImp
 from service_layer.abstract_classes.create_post_service import CreatePostService
@@ -15,8 +17,17 @@ def test_create_post_image_service_success():
 
 
 def test_create_post_image_service_failure_not_int():
-    pass
+    try:
+        create_post_service.create_post_image_service("hello", "thisisaforwardvalue")
+        assert False
+    except PostIdMustBeAnInteger as e:
+        assert str(e) == "The post id must be an integer."
 
 
 def test_create_post_image_service_failure_not_str():
-    pass
+    try:
+        create_post_service.create_post_image_service(1, b"thisisaforwardvalue")
+        assert False
+    except ImageMustBeAString as e:
+        assert str(e) == "The image must be a string format."
+
