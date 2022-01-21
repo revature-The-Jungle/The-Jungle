5c0091965bcb75ffb53bc5703681598c9221a951
Chris24xx <94495502+Chris24xx@users.noreply.github.com>
2022-01-21 17:30:03 -0500

    tests done in group_member_junction_dao.py for user list and leaving group as well as test

--- PythonAPI/data_access_layer/implementation_classes/group_member_junction_dao.py
+++ PythonAPI/data_access_layer/implementation_classes/group_member_junction_dao.py
@@ -5,6 +5,7 @@ from PythonAPI.data_access_layer.abstract_classes.group_member_junction_abs impo
 from PythonAPI.entities import group_member_junction
 from PythonAPI.entities.group_member_junction import GroupMemberJunction
 from PythonAPI.util.database_connection import connection
+from custom_exceptions.group_member_junction_exceptions import *
 
 
 class GroupMemberJunctionDao(GroupMemberJunctionAbs):
@@ -21,16 +22,23 @@ class GroupMemberJunctionDao(GroupMemberJunctionAbs):
             for member in group_record:
                 group_list.append(GroupMemberJunction(*member))
             return group_list
-        except TypeError as e:
-            print("you have input the incorrect amount of arguments")
+        except AssertionError:
+            raise AssertionError("This is not allowed")
+        except TypeError:
+            raise TypeError("you have put the incorrect amount of arguments")
 
     # deletes user from group_member_junction_table
     def leave_group(self, user_id: int, group_id: int):
-        try:
-            sql = "delete from group_member_junction_table where user_id = %s and group_id = %s"
-            cursor = connection.cursor()
-            cursor.execute(sql, [user_id, group_id])
-            connection.commit()
-            return True
-        except TypeError as e:
-            print("please check your arguments")
+        check_list = self.get_all_users_in_a_group()
+        for checks in check_list:
+            if checks.group_id == group_id:
+                if checks.user_id == user_id:
+                    try:
+                        sql = "delete from group_member_junction_table where user_id = %s and group_id = %s"
+                        cursor = connection.cursor()
+                        cursor.execute(sql, [user_id, group_id])
+                        connection.commit()
+                        return True
+                    except TypeError:
+                        raise TypeError("too many arguments")
+        raise WrongId("Incorrect ID")
--- PythonAPI/tests_for_app/test_for_data_access_layer/test_group_member_junction_dao.py
+++ PythonAPI/tests_for_app/test_for_data_access_layer/test_group_member_junction_dao.py
@@ -1,5 +1,8 @@
+import pytest
+
 from PythonAPI.data_access_layer.implementation_classes.group_member_junction_dao import GroupMemberJunctionDao
 from PythonAPI.entities.group_member_junction import GroupMemberJunction
+from custom_exceptions.group_member_junction_exceptions import *
 
 group_member_junction_dao = GroupMemberJunctionDao()
 
@@ -16,11 +19,30 @@ def test_list_contains_correct_info():
     assert mem.first_name == "test create post"
 
 
+def test_to_many():
+    with pytest.raises(TypeError) as e:
+        group_member_junction_dao.get_all_users_in_a_group(1)
+        assert "you have put the incorrect amount of arguments" in str(e.value)
+
+
 def test_leave_group():
-    result = group_member_junction_dao.leave_group(10000, 4)
+    result = group_member_junction_dao.leave_group(10000, 9)
     assert result
 
 
 def test_leave_group2():
-    result = group_member_junction_dao.leave_group(9000, 4)
+    result = group_member_junction_dao.leave_group(10000, 10)
     assert result
+
+
+def test_failed_leave_group():
+    with pytest.raises(TypeError) as e:
+        group_member_junction_dao.leave_group(16, 10, 5)
+        assert "too many arguments" in str(e.value)
+
+
+def test_wrong_user():
+    try:
+        group_member_junction_dao.leave_group(1, 1)
+    except WrongId as e:
+        assert str(e) == "Incorrect ID"
