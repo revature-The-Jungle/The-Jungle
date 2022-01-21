fd34072675fb1e87923bb2da0bbdcc59496b8cad
Irfan Uludag <ozgr329@gmail.com>
2022-01-21 14:48:20 -0500

    Added ChatController

--- JavaAPI/src/main/java/dev/com/thejungle/app/app/App.java
+++ JavaAPI/src/main/java/dev/com/thejungle/app/app/App.java
@@ -1,18 +1,17 @@
 package dev.com.thejungle.app.app;
-
-import dev.com.thejungle.app.appcontroller.appcontroller.AppController;
+import dev.com.thejungle.app.appcontroller.controllers.ChatController;
 import io.javalin.Javalin;
 
 public class App {
 
     public static void main(String[] args) {
-
         Javalin app = Javalin.create(config -> {
             config.enableCorsForAllOrigins();
             config.enableDevLogging();
         });
 
-        AppController appController = new AppController();
+
+        app.ws("/chat/{id}", ChatController::connectToWebSocket);
 
         app.start();
 
--- JavaAPI/src/main/java/dev/com/thejungle/app/appcontroller/controllers/ChatController.java
+++ JavaAPI/src/main/java/dev/com/thejungle/app/appcontroller/controllers/ChatController.java
@@ -1,20 +1,19 @@
 package dev.com.thejungle.app.appcontroller.controllers;
 
+import com.google.gson.Gson;
 import dev.com.thejungle.dao.implementations.ChatDAO;
+import dev.com.thejungle.entity.ChatMessage;
 import dev.com.thejungle.service.implementations.ChatService;
-import io.javalin.http.Handler;
+import io.javalin.websocket.WsConfig;
 import io.javalin.websocket.WsContext;
-import org.asynchttpclient.netty.handler.WebSocketHandler;
-import org.eclipse.jetty.websocket.server.WebSocketHandshake;
-
-import java.net.http.WebSocket;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatController {
 
 
-    private static Map<WsContext, String> userUsernameMap = new ConcurrentHashMap<>();
+    public static Map<WsContext, String> userUsernameMap = new ConcurrentHashMap<>();
 
 
     ChatService chatService;
@@ -23,20 +22,36 @@ public class ChatController {
         this.chatService = chatService;
     }
 
-//    public WebSocketHandler connectToChat = ws -> {
-//        ws.onConnect(ctx -> {
-//
-//            String username = "User"  ;
-//            userUsernameMap.put(ctx, ctx.pathParam("id"));
-//            broadcastMessage(userUsernameMap.get(ctx), ctx.getSessionId() ,ctx.pathParam("id"));
-//        });
-//        ws.onClose(ctx -> {
-//            String username = userUsernameMap.get(ctx);
-//            userUsernameMap.remove(ctx);
-//            broadcastMessage(userUsernameMap.get(ctx), ctx.getSessionId() ,ctx.pathParam("id"));
-//        });
-//        ws.onMessage(ctx -> {
-//            broadcastMessage(userUsernameMap.get(ctx), ctx.message() ,ctx.pathParam("id"));
-//        });
-//    };
+    public static void connectToWebSocket(WsConfig ws){
+        ws.onConnect(ctx -> {
+            String username = "User"  ;
+            userUsernameMap.put(ctx, ctx.pathParam("id"));
+            broadcastMessage(userUsernameMap.get(ctx), "has connected to chat" );
+
+        });
+        ws.onClose(ctx -> {
+            String username = userUsernameMap.get(ctx);
+            userUsernameMap.remove(ctx);
+            broadcastMessage(userUsernameMap.get(ctx), ctx.getSessionId() );
+        });
+        ws.onMessage(ctx -> {
+            Gson gson = new Gson();
+            ChatMessage messageJson = gson.fromJson(ctx.message(), ChatMessage.class);
+            chatService.serviceCreateMessage(messageJson);
+            broadcastMessage(userUsernameMap.get(ctx), ctx.message());
+        });
+    };
+
+    public static void broadcastMessage(String senderId, String message) {
+
+        userUsernameMap.keySet().stream().filter(ctx -> (ctx.session.isOpen() && userUsernameMap.get(ctx) == senderId)).forEach(session -> {
+            System.out.println(session);
+            Gson gson = new Gson();
+            Map<String, Object> broadcastString = new HashMap<>();
+            broadcastString.put("userName", sender);
+            broadcastString.put("userMessage", message);
+            broadcastString.put("userList", userUsernameMap.values());
+            session.send(gson.toJson(broadcastString));
+        });
+    }
 }
