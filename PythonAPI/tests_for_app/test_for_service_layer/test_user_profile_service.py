e7f211004c9cc2052b1520f18cfac3552b2a839c
dZazulak <94470265+dZazulak@users.noreply.github.com>
2022-01-21 11:55:51 -0600

    Update test_user_profile_service.py
    changed name of method in service tests

--- PythonAPI/tests_for_app/test_for_service_layer/test_user_profile_service.py
+++ PythonAPI/tests_for_app/test_for_service_layer/test_user_profile_service.py
@@ -36,7 +36,7 @@ def test_update_user_profile_service_failure_too_many_chars():
         assert str(e) == "Too many characters."
 
 
-def test_update_user_profile_service_failure_birth_date_not_null():
+def test_update_user_profile_service_failure_birth_date_is_null():
     updated_user_fail_birth_date: User = User(1, "test_first_name", "test_last_name", "test@test.com", "test_username",
                                       "test_passcode", "About me test", None, "Test image")
     try:
