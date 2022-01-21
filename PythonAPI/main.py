d1367bae6115ac8e28c6171ac4b2b51901517975
ericthered1138 <ericjennings955@gmail.com>
2022-01-20 22:32:13 -0600

    Update main.py

--- PythonAPI/main.py
+++ PythonAPI/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from custom_exceptions.image_must_be_a_string import ImageMustBeAString
+from custom_exceptions.post_id_must_be_an_integer import PostIdMustBeAnInteger
+from custom_exceptions.post_not_found import PostNotFound
 from data_access_layer.implementation_classes.create_post_dao_imp import CreatePostDAOImp
 from service_layer.implementation_classes.create_post_service_imp import CreatePostServiceImp
 
@@ -24,11 +27,18 @@ def on():
 
 
 @app.post("/post/image/<post_id>")
-def post_a_post_image(post_id):
-    """Needs a lot more work. Proof of concept regarding creating images in the database."""
-    image = request.data
-    image_decoded = image.decode('utf-8')
-    return create_post_service.create_post_image_service(post_id, image_decoded)
+def create_a_post_image(post_id):
+    """Method to insert an image into the database. Returns the same image back from the database."""
+    try:
+        image = request.data
+        image_decoded = image.decode('utf-8')
+        return create_post_service.create_post_image_service(post_id, image_decoded)
+    except PostIdMustBeAnInteger as e:
+        return str(e), 400
+    except ImageMustBeAString as e:
+        return str(e), 400
+    except PostNotFound as e:
+        return str(e), 400
 
 
 app.run()
