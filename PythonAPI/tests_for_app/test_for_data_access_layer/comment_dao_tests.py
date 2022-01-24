6fea4b25c4ab60cf7b7589c869888a1d1963853e
Mohammad <moh.bahrami1989@gmail.com>
2022-01-24 14:38:45 -0600

    comment tests added

--- PythonAPI/tests_for_app/test_for_data_access_layer/comment_dao_tests.py
+++ PythonAPI/tests_for_app/test_for_data_access_layer/comment_dao_tests.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from custom_exceptions.comment_exceptions import CommentNotFound, NoPostFound, CommentNotCreated
 from data_access_layer.abstract_classes import comment_dao
 from data_access_layer.abstract_classes.comment_dao import CommentDAO
 from data_access_layer.implementation_classes.comment_dao_imp import CommentDAOImp
@@ -8,33 +9,63 @@ from entities import comment
 comment_dao = CommentDAOImp()
 
 
-user_id = 1;
+# Create Comment Tests--------------------------------------------
+# Happy Path Test 1
+def test_create_comment_success():
+    created_comment = comment_dao.create_comment(3, 1, "hello world", 1, 1)
+    assert created_comment.comment_id != 0
 
-def test_create_comment_success1():
-    id = comment_dao.create_comment(1, 1, "hello world", 1, 1)
-    assert id != 0
+# Happy Path Test 2
+def test_create_comment_success_2():
+    try:
+         created_comment = comment_dao.create_comment(3, 1, " ", 1, 1)
+    except CommentNotCreated as e:
+        assert str(e) == "Comment Not Created"
 
-def test_create_comment_success2():
-    id = comment_dao.create_comment(1, 11, "Good Bye", 2, 1)
-    assert id != 0
+# Sad Path Test
+# Test Needs to be reworked.
+#     def test_create_comment_fail():
 
 
-
-def test_delete_comment_success1():
+# Delete Comment Tests--------------------------------------------
+# Happy Path Test 1
+def test_delete_comment_success():
     result = comment_dao.delete_comment(4)
     assert result == True
 
 
-def test_delete_comment_success2():
-    result = comment_dao.delete_comment(14)
-    assert result == True
+# Happy Path Test 2
+def test_delete_comment_success_2():
+    result = comment_dao.delete_comment(9)
+    assert result != False
 
 
-def test_to_get_all_comments_by_post_id1():
+# Test delete comment failed
+def test_delete_comment_by_id_fail_comment_not_found():
+    try:
+        comment_dao.delete_comment(1000)
+    except CommentNotFound as e:
+        assert str(e) == "Comment Not Found"
+
+
+# ------------------------End Delete Comment
+
+# Get All Comment By Post Id Tests--------------------------------------------
+# Happy Path Test 1
+def test_to_get_all_comments_by_post_id_list_success_1():
     returned_comments = comment_dao.get_comment_by_post_id(3)
-    assert len(returned_comments) == 3
+    assert len(returned_comments) != 0
 
 
-def test_to_get_all_comments_by_post_id2():
-    returned_comments = comment_dao.get_comment_by_post_id(12)
-    assert len(returned_comments) == 1
+# Happy Path Test 2
+def test_to_get_all_comments_by_post_id_list_success_2():
+    returned_comments = comment_dao.get_comment_by_post_id(3)
+    assert len(returned_comments) >= 1
+
+
+# Test post does not exist to place comment
+def test_no_post_found():
+    try:
+        returned_comments = comment_dao.get_comment_by_post_id(300)
+    except NoPostFound as e:
+        assert str(e) == "No Post Found"
