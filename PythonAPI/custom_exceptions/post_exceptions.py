8520fda11b161b31b195910e706477dfde4177aa
LocPhanRevature <loc.phan@revature.net>
2022-01-24 15:40:36 -0600

    update

--- PythonAPI/custom_exceptions/post_exceptions.py
+++ PythonAPI/custom_exceptions/post_exceptions.py
@@ -1,3 +1,8 @@
 class PostNotFound(Exception):
     def __init__(self, message):
         self.message = message
+
+
+class InvalidInput(Exception):
+    def __init__(self, message):
+        self.message = message
