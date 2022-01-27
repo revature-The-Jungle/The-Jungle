9d1c86f32e9765c9e4b0433b444043cd5a0b8cc7
ericthered1138 <ericjennings955@gmail.com>
2022-01-26 21:36:11 -0600

    punched up the group_dao methods

--- PythonAPI/data_access_layer/abstract_classes/group_dao_abs.py
+++ PythonAPI/data_access_layer/abstract_classes/group_dao_abs.py
@@ -11,7 +11,3 @@ class GroupDAO(ABC):
     @abstractmethod
     def join_group(self, group_id: int, user_id: int):
         pass
-
-    @abstractmethod
-    def get_creator(self,group_id:int):
-        pass
