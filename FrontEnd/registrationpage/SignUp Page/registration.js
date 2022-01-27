b291523bd81da6a78d490d0665611145ffc0f27d
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-01-27 13:43:19 -0800

    Finished input validation for website js.

--- FrontEnd/registrationpage/SignUp Page/registration.js
+++ FrontEnd/registrationpage/SignUp Page/registration.js
@@ -1,3 +1,6 @@
+const signupSubmitButton = document.getElementById("signup-submit");
+signupSubmitButton.disabled = true;
+
 const signupFirstName = document.getElementById("signup-firstname");
 const signupLastName = document.getElementById("signup-lastname");
 const signupEmail = document.getElementById("signup-email");
@@ -5,62 +8,227 @@ const signupBirthdate = document.getElementById("signup-bdate");
 const signupUsername = document.getElementById("signup-username");
 const signupPassword = document.getElementById("signup-password");
 const signupPasswordRepeat = document.getElementById("signup-repeat-password");
+const invalidIcon = document.querySelectorAll("[id='invalid-icon']");
+let invalidMessage = document.querySelectorAll("[id='signup-invalid-message']");
+let validateCounter = 0;
 
+const specialChar1 = /[ `!@#$%^&*()_+=\[\]{};':"\\|,.<>\/?~]/;
+const specialChar2 = /[ `^*()+=\[\]{};':"\\|,<>\/~]/;
 
-function getBirthDateInput(){
-
+let jsonUserObject = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    passcode: "",
+    userBirthdate: 0
 };
 
 
-let invalidIcon = document.getElementById("invalid-icon");
-let invalidMessageFirstName = document.getElementById("signup-invalid-message-firstname");
-let invalidMessageLastName = document.getElementById("signup-invalid-message-lastname");
-let invalidMessageEmail = document.getElementById("signup-invalid-message-email");
-let invalidMessageBDay = document.getElementById("signup-invalid-message-bday");
-let invalidMessageUsername = document.getElementById("signup-invalid-message-username");
-let invalidMessagePassword = document.getElementById("signup-invalid-message-password");
-let invalidMessagePasswordRepeat = document.getElementById("signup-invalid-message-repeat-password");
-
-
-
-
-function validateSignUpInput(){
-    if (signupFirstName.value == "") {
-        invalidIcon.style.display = "";
-        invalidMessageFirstName.textContent = 'First name is missing.';
-    }
-    else if (signupLastName.value == "") {
-        invalidIcon.style.display = "";
-        invalidMessageLastName.textContent = 'Last name is missing.';
-    }
-    else if (signupEmail.value == "") {
-        invalidIcon.style.display = "";
-        invalidMessageEmail.textContent = 'Email is missing.';
-    }
-    else if (signupBirthdate.value == "") {
-        invalidIcon.style.display = "";
-        invalidMessageBDay.textContent = 'Birthday is missing.'
-    }
-    else if (signupUsername.value == "") {
-        invalidIcon.style.display = "";
-        invalidMessageUsername.textContent = 'Username is missing.'
-    }
-    else if (signupPassword.value == "") {
-        invalidIcon.style.display = "";
-        invalidMessagePassword.textContent = 'Password is missing.'
-    }
-    else if (signupPasswordRepeat.value == "") {
-        invalidIcon.style.display = "";
-        invalidMessagePasswordRepeat.textContent = 'Matching password is missing.'
-    }
-    else {
-        if (signupPassword.value != signupPasswordRepeat.value) {
-            invalidIcon.style.display = "";
-            invalidMessagePassword.textContent = 'Passwords do not match!';
-            invalidMessagePasswordRepeat.textContent = 'Passwords do not match!';
+// VALIDATE ALL THE INPUTS
+// Validate first name input.
+signupFirstName.addEventListener("focusin", validateFirstName());
+function validateFirstName(){
+    signupFirstName.addEventListener("focusout", function() {
+        if (signupFirstName.value == "") {
+            invalidIcon[0].style.display = "";
+            invalidMessage[0].textContent = 'First name is missing.';
+        }
+        else if (specialChar1.test(signupFirstName.value)){
+            invalidIcon[0].style.display = "";
+            invalidMessage[0].textContent = 'Cannot contain spaces or special characters.';
         }
         else {
-            registerUser();
+            invalidIcon[0].style.display = "none";
+            invalidMessage[0].textContent = '';
+            jsonUserObject.firstName = signupFirstName.value;
+            validateCounter = validateCounter + 1;
+            // console.log(jsonUserObject.firstName);
         }
-    }
+    });
 };
+
+
+// Validate last name input.
+signupLastName.addEventListener("focusin", validateLastName());
+function validateLastName(){
+    signupLastName.addEventListener("focusout", function() {
+        if (signupLastName.value == "") {
+            invalidIcon[1].style.display = "";
+            invalidMessage[1].textContent = 'Last name is missing.';
+        }
+        else if (specialChar1.test(signupLastName.value)){
+            invalidIcon[1].style.display = "";
+            invalidMessage[1].textContent = 'Cannot contain spaces or special characters.';
+        }
+        else {
+            invalidIcon[1].style.display = "none";
+            invalidMessage[1].textContent = '';
+            jsonUserObject.lastName = signupLastName.value;
+            validateCounter = validateCounter + 1;
+            // console.log(jsonUserObject.lastName);
+        }
+    });
+};
+
+
+// Validate email input.
+signupEmail.addEventListener("focusin", validateEmail());
+function validateEmail(){
+    signupEmail.addEventListener("focusout", function() {
+        if (signupEmail.value == "") {
+            invalidIcon[2].style.display = "";
+            invalidMessage[2].textContent = 'Email is missing.';
+        }
+        else if (specialChar2.test(signupEmail.value)){
+            invalidIcon[2].style.display = "";
+            invalidMessage[2].textContent = 'Cannot contain spaces or special characters: `^*()+=[]{}"<>~|;:';
+        }
+        else {
+            invalidIcon[2].style.display = "none";
+            invalidMessage[2].textContent = '';
+            jsonUserObject.email = signupEmail.value;
+            validateCounter = validateCounter + 1;
+            // console.log(jsonUserObject.email);
+        }
+    });
+};
+
+
+
+// Validate birthday input.
+signupBirthdate.addEventListener("focusin", validateBirthdate());
+function validateBirthdate(){
+    signupBirthdate.addEventListener("focusout", function() {
+        if (signupBirthdate.value == "") {
+            invalidIcon[3].style.display = "";
+            invalidMessage[3].textContent = 'Birthdate is missing.';
+        }
+        else {
+            invalidIcon[3].style.display = "none";
+            invalidMessage[3].textContent = '';
+            let userBDay = new Date(signupBirthdate.value).getTime();
+            jsonUserObject.userBirthdate = userBDay;
+            validateCounter = validateCounter + 1;
+            // console.log(jsonUserObject.userBirthdate);
+        }
+    });
+};
+
+
+// Validate username input.
+signupUsername.addEventListener("focusin", validateUsername());
+function validateUsername(){
+    signupUsername.addEventListener("focusout", function() {
+        if (signupUsername.value == "") {
+            invalidIcon[4].style.display = "";
+            invalidMessage[4].textContent = 'Username is missing.';
+        }
+        else if (specialChar2.test(signupUsername.value)){
+            invalidIcon[4].style.display = "";
+            invalidMessage[4].textContent = 'Cannot contain spaces or special characters: `^*()+=[]{}"<>~|;:';
+        }
+        else {
+            invalidIcon[4].style.display = "none";
+            invalidMessage[4].textContent = '';
+            jsonUserObject.username = signupUsername.value;
+            validateCounter = validateCounter + 1;
+            // console.log(jsonUserObject.username);
+        }
+    });
+};
+
+
+
+// Validate first password input.
+signupPassword.addEventListener("focusin", validatePassword());
+function validatePassword(){
+    signupPassword.addEventListener("focusout", function() {
+        if (signupPassword.value == "") {
+            invalidIcon[5].style.display = "";
+            invalidMessage[5].textContent = 'Password is missing.';
+        }
+        else if (specialChar2.test(signupPassword.value)){
+            invalidIcon[5].style.display = "";
+            invalidMessage[5].textContent = 'Cannot contain spaces or special characters: `^*()+=[]{}"<>~|;:';
+        }
+        else {
+            invalidIcon[5].style.display = "none";
+            invalidMessage[5].textContent = '';
+            let validatedPass = signupPassword.value;
+            // console.log(validatedPass);
+            signupPasswordRepeat.addEventListener("focusin", validateRepeatPassword(validatedPass));
+        }
+    });
+};
+
+
+// Validate both passwords match.
+function validateRepeatPassword(password){
+    signupPasswordRepeat.addEventListener("focusout", function() {
+        if (signupPasswordRepeat.value != password) {
+            invalidIcon[6].style.display = "";
+            invalidMessage[6].textContent = 'Passwords do not match.';
+        }
+        else {
+            invalidIcon[6].style.display = "none";
+            invalidMessage[6].textContent = '';
+            jsonUserObject.passcode = password;
+            validateCounter = validateCounter + 1;
+            // console.log(jsonUserObject.passcode);
+            // console.log(validateCounter);
+            if(validateCounter > 5){
+                signupSubmitButton.disabled = false;
+            }
+        }
+    });
+};
+
+
+
+// let jsonUserObject = {
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     username: "",
+//     passcode: "",
+//     userBirthdate: 0
+// };
+
+
+// ROUTE TO REGISTER/CREATE USER
+signupSubmitButton.addEventListener("click", registerUser);
+async function registerUser(){
+    const registerRoute = "http://localhost:8080/user/registration";
+}
+
+// async function createMedia(){
+//     let mediaType = document.getElementById("media-type");
+//     let mediaTitle = document.getElementById("media-title");
+
+//     const createRoute = "http://localhost:8080/newMedia";
+//     let response = await fetch(createRoute, {headers:{'Content-Type':'application/json'}, method:["POST"], body:JSON.stringify({"mediaId":0, 
+//                     "title":mediaTitle.value, 
+//                     "creator":mediaCreator.value, 
+//                     "synopsis":mediaSynopsis.value, 
+//                     "mediaType": mediaType.value, 
+//                     "genre":mediaGenre.value, 
+//                     "status":false, 
+//                     "userId": sessionStorage.getItem("webUserId")}) });
+//     if(response.status == 201){
+//         let createdMediaBody = await response.json();
+//         mediaType.value = '';
+//         mediaTitle.value = '';
+//         mediaCreator.value = '';
+//         mediaGenre.value = '';
+//         mediaSynopsis.value = '';
+//         submitMessage.textContent = 'Your media has been added and is awaiting approval.';
+//     } else {
+//         submitMessage.textContent = 'There was an issue. Your media was not added.';
+//     };
+// };
+
+
+
+
+
