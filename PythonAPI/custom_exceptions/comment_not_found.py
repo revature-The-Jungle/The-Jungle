8eb9a1c63e93a51543a24c88e829abc124293bc0
VictorFloresHerrera <victor.floresherrera@revature.net>
2022-01-27 15:49:03 -0600

    null

--- PythonAPI/custom_exceptions/comment_not_found.py
+++ PythonAPI/custom_exceptions/comment_not_found.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD:PythonAPI/custom_exceptions/group_name_already_taken.py
-class GroupNameAlreadyTaken(Exception):
-=======
+
+#class GroupNameAlreadyTaken(Exception):
+
 class CommentNotFound(Exception):
->>>>>>> origin/eJennings/Python:PythonAPI/custom_exceptions/comment_not_found.py
+
     def __init__(self, message):
         self.message = message
