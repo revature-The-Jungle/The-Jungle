f018945125a83f8e598396f93739777b0b0eba9e
VictorFloresHerrera <victor.floresherrera@revature.net>
2022-01-28 16:46:31 -0600

    html & JS user profile image

--- PythonAPI/custom_exceptions/comment_not_found.py
+++ PythonAPI/custom_exceptions/comment_not_found.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD:PythonAPI/custom_exceptions/group_not_found.py
-class GroupNotFound(Exception):
-=======
+# class GroupNotFound(Exception):
+
 class CommentNotFound(Exception):
->>>>>>> origin/eJennings/Python:PythonAPI/custom_exceptions/comment_not_found.py
+
     def __init__(self, message):
         self.message = message
