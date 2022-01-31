dfeca240f8d3eb13ab94ea5301bee499524019dd
dZazulak <94470265+dZazulak@users.noreply.github.com>
2022-01-31 13:08:52 -0600

    updated
    Added functions in the javascript and set source to default for users/groups without an image, changed css for pictures to have border-radiuses

--- FrontEnd/profilepage/dZazulak.js
+++ FrontEnd/profilepage/dZazulak.js
@@ -72,7 +72,7 @@ async function getUserFollowers(){
 
     if(response.status === 200){
         let body = await response.json();
-        console.log(body);
+        // console.log(body);
         populateUserFollowers(body);
         getFollowerImage(body);
     }
@@ -90,7 +90,9 @@ function populateUserFollowers(followerBody){
         // Create the image tag and set class name
         let followerImage = document.createElement("img");
         followerImage.setAttribute("class", "friend");
-        followerImage.setAttribute("id", "follower-image");
+        followerImage.setAttribute("id", `${follower}-image`);
+        followerImage.setAttribute("alt", "No Image");
+        followerImage.setAttribute("src", "img/default-profile-picture.jpg");
 
         // Created the username div and set the class name and username
         let followerUsernameDiv = document.createElement("div");
@@ -105,6 +107,20 @@ function populateUserFollowers(followerBody){
     }
 }
 
+async function getFollowerImage(followerBody){
+    for(follower in followerBody){
+        let image_Element = document.getElementById(`${follower}-image`);
+        let url = `http://127.0.0.1:5000/user/image/${followerBody[follower]}`;
+        console.log(url);
+        let response = await fetch(url);
+        if(response.status === 200){
+            const image_text = await response.text();
+            image_Element.src = image_text;
+        }
+
+}
+}
+
 async function getGroupsForUser(){
     let url = "http://127.0.0.1:5000/group/user/10"
 
@@ -112,7 +128,7 @@ async function getGroupsForUser(){
 
     if(response.status === 200){
         let body = await response.json();
-        console.log(body);
+        // console.log(body);
         populateGroupsForUsers(body);
     }
     else{
@@ -127,10 +143,12 @@ function populateGroupsForUsers(groupBody){
 
         let groupImage = document.createElement("img");
         groupImage.setAttribute("class", "friend");
+        groupImage.setAttribute("alt", "No Image");
+        groupImage.setAttribute("src", "img/default-profile-picture.jpg");
 
         let groupNameDiv = document.createElement("div");
         groupNameDiv.setAttribute("class", "name valign-text-middle poppins-bold-astronaut-22px");
-        groupNameDiv.innerHTML = `<a id="groupLink-" + ${groupBody[group].groupId} class="name valign-text-middle poppins-bold-astronaut-22px" onclick=goToGroupPage(${groupBody[group].groupId}) href="../individualgrouppage/individual-group-page.html">${groupBody[group].groupName}</a>`;
+        groupNameDiv.innerHTML = `<a id="groupLink-${groupBody[group].groupId}" class="name valign-text-middle poppins-bold-astronaut-22px" onclick=goToGroupPage(${groupBody[group].groupId}) href="../individualgrouppage/individual-group-page.html">${groupBody[group].groupName}</a>`;
         groupSectionDiv.appendChild(groupsDiv);
         groupsDiv.appendChild(groupImage);
         groupsDiv.appendChild(groupNameDiv);
@@ -139,24 +157,18 @@ function populateGroupsForUsers(groupBody){
     }
 }
 
-async function getFollowerImage(followerBody){
-    for(follower in followerBody){
-        let url = "http://127.0.0.1:5000/user/image/" + userId;
-        let response = await fetch(url);
-        console.log(response);
-        if(response.status === 200){
-            const image_text = await response.text();
-            let image_Element = document.getElementById("follower-image");
-            image_Element.src = image_text;
-        
+async function getGroupImage(groupBody){
+    for(group in groupBody){
+        let imageElement = document.getElementById(`${groupBody[group].groupName}-image`);
+        let url = ""
     }
-}
 }
 
 function goToGroupPage(groupId){
     let groupLink = getElementById("groupLink-" + groupId);
     // groupLink.setAttribute("href", "")
     localStorage.setItem("groupId") = groupId;
+    localStorage.getItem("groupId");
 }
 
 getUserFollowers();
