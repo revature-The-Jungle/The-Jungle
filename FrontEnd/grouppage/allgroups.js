c25caadeadd6413923bf1f0a7e9ae3785282b88a
Dearce85 <dearce.goodman@revature.net>
2022-01-31 14:29:44 -0500

    update to the groups page

--- FrontEnd/grouppage/allgroups.js
+++ FrontEnd/grouppage/allgroups.js
@@ -1,28 +1,43 @@
-const table = document.getElementById("groupTable")
-const tableBody = document.getElementById("groupBody")
 
-async function getAllGroupData(){
-    let url = "http://127.0.0.1:5000/group";
+const groupSectionDiv = document.getElementById("groups-div");
+
+
+
+
+
+async function getAllGroupsForUser(){
+    let url = "http://127.0.0.1:5000/group"
 
     let response = await fetch(url);
-    
-    if (response.status === 200){
+
+    if(response.status === 200){
         let body = await response.json();
         console.log(body);
-        populateData(body);  
-    }else {
-        alert("Problem loading group data");
+        populateGroupsForUsers(body);
+    }
+    else{
+        alert("Error with groups");
     }
 }
 
-function populateData(responseBody){
+function populateGroupsForUsers(groupBody){
+    for (let group in groupBody){
+        let groupsDiv = document.createElement("div");
+        groupsDiv.setAttribute("class", "group-in-list");
+
+        let groupImage = document.createElement("img");
+        groupImage.setAttribute("class", "friend");
+
+        let groupNameDiv = document.createElement("div");
+        groupNameDiv.setAttribute("class", "name valign-text-middle poppins-bold-astronaut-22px");
+        groupNameDiv.innerHTML = groupBody[group].groupName;
+
+        groupSectionDiv.appendChild(groupsDiv);
+        groupsDiv.appendChild(groupImage);
+        groupsDiv.appendChild(groupNameDiv);
 
 
-    for (let group of responseBody){
-        let tableRow = document.createElement("tr");
-        tableRow.innerHTML = `<td>${group.groupName}</td>`;
-        tableBody.appendChild(tableRow);
     }
 }
-getAllGroupData()
 
+getAllGroupsForUser();
