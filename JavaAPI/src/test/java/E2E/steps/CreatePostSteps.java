1f4c8feb41d6852fd2b150d59e6deed425c94141
ericthered1138 <ericjennings955@gmail.com>
2022-02-01 10:30:00 -0600

    added feature files

---
+++ JavaAPI/src/test/java/E2E/steps/CreatePostSteps.java
@@ -0,0 +1,32 @@
+package E2E.steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CreatePostSteps {
+
+
+    @When("the user upload his photo")
+    public void the_user_upload_his_photo() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters his post body")
+    public void the_user_enters_his_post_body() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user clicks on the post button")
+    public void the_user_clicks_on_the_post_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user will see the created post")
+    public void the_user_will_see_the_created_post() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    }
---
+++ JavaAPI/src/test/java/resources/createPosts.feature
@@ -0,0 +1,8 @@
+Feature: Create Post
+
+Scenario: As a user I want to create a post
+Given the user is on the homepage
+When the user upload his photo
+When the user enters his post body
+When the user clicks on the post button
+Then the user will see the created post
