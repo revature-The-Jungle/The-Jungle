cd388ff901d1d6eead0716fe4da0cbd550ca5174
ericthered1138 <ericjennings955@gmail.com>
2022-02-01 11:11:23 -0600

    added scenarios

--- JavaAPI/src/test/java/E2E/steps/CreatePostSteps.java
+++ JavaAPI/src/test/java/E2E/steps/CreatePostSteps.java
@@ -4,22 +4,20 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CreatePostSteps {
-
     @When("the user clicks on the create post modal button")
     public void the_user_clicks_on_the_create_post_modal_button() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-
-    @When("the user upload his photo")
-    public void the_user_upload_his_photo() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("the user enters his post body")
-    public void the_user_enters_his_post_body() {
+    @When("the user upload their photo")
+    public void the_user_upload_their_photo() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters their post body")
+    public void the_user_enters_their_post_body() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
@@ -32,6 +30,24 @@ public class CreatePostSteps {
 
     @Then("the user will see the created post")
     public void the_user_will_see_the_created_post() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user upload their photo that is too large")
+    public void the_user_upload_their_photo_that_is_too_large() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user will see an error message in the post feed")
+    public void the_user_will_see_an_error_message_in_the_post_feed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters their post body with too much text")
+    public void the_user_enters_their_post_body_with_too_much_text() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
